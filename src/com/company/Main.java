37009a58f39d88819aeae206443cd5b83b381d5a
kairoboss <kairoboss125@gmail.com>
2020-06-14 15:06:47 +0600

    попытка 2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,14 +4,14 @@ public class Main {
 
     public static void main(String[] args) {
         double[] numbers = {58.5, 67.7, -15.8, 67.8, -89.7, 73.7, 12.5, -1.5, 89.9, 54.3, -34.4, 73.3, 67.6, 99.9, 90.1};
-        double average = 0;
-        for (double digit : numbers) {
-            average += digit;
+       double average = 0;
+       double digit = 0;
+       for (int i = 2; i >=2 && i<15; i++) {
+            digit += numbers[i];
 
         }
-        System.out.println(average / numbers.length);
-
-
+        average = digit/13;
+        System.out.println(average);
     }
 
 
