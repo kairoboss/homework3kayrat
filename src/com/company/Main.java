322b2a7633fcac66da3b34082dafd78997a907f9
kairoboss <kairoboss125@gmail.com>
2020-06-14 21:39:52 +0600

    попытка 3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,18 +1,24 @@
 package com.company;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class Main {
 
     public static void main(String[] args) {
         double[] numbers = {58.5, 67.7, -15.8, 67.8, -89.7, 73.7, 12.5, -1.5, 89.9, 54.3, -34.4, 73.3, 67.6, 99.9, 90.1};
-       double average = 0;
-       double digit = 0;
-       for (int i = 2; i >=2 && i<15; i++) {
-            digit += numbers[i];
+        double average = 0;
 
+
+        for (double digit : numbers) {
+            if (digit == 58.5 || digit == 67.7) {
+                continue;
+            }
+            average += digit;
         }
-        average = digit/13;
-        System.out.println(average);
+        System.out.println(average/numbers.length);
     }
 
 
 }
+
+
